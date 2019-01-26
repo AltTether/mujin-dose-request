f415247e613a84964bd1384d518e6610a870eb2e
AltTether <200351p@ugs.kochi-tech.ac.jp>
2019-01-26 15:02:52 +0900

    get,postでpoolのリクエストを実行可能に

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 import json
+
 from flask import Flask, request
+from src import Pool
 
 
 app = Flask(__name__)
+pool = Pool()
 
-@app.route('/', methods=['post'])
+@app.route('/', methods=['GET','POST'])
 def main():
-    hoge = request.json
-    return json.dumps(hoge)
+    response = None
+    if request.method == 'GET':
+        response = pool.pop()
+    elif request.method == 'POST':
+        body_json = request.json
+        pool.push(body_json)
+        response = {'result': 'success'}
+    return json.dumps(response)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
