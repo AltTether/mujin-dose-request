7e214d37e4bae219c0c4d824ae972f0cfa02b30a
AltTether <200351p@ugs.kochi-tech.ac.jp>
2019-01-26 14:43:14 +0900

    postしたjsonをダンプして返す

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+import json
+from flask import Flask, request
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['post'])
 def main():
-    return "hello"
+    hoge = request.json
+    return json.dumps(hoge)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
